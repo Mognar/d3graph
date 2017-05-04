56f8819a35d95e36bb7aad0e94bc7c2f6db78921
Mognar <philipbgorman@gmail.com>
2017-05-04 12:26:23 +0100

    Update d3.layout.js

--- d3.layout.js
+++ d3.layout.js
@@ -501,7 +501,7 @@ function d3_layout_forceDragOut(d) {
 
 function d3_layout_forceDragEnd() {
   d3_layout_forceDrag();
-  d3_layout_forceDragNode.fixed &= 2;
+  d3_layout_forceDragNode.fixed &= 1;
   d3_layout_forceDragForce = d3_layout_forceDragNode = null;
 }
 
